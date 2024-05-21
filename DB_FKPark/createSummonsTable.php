dc01581bc72c8558c049f8da78e67c76701612c3
ChunkyBoyz77 <165290910+ChunkyBoyz77@users.noreply.github.com>
2024-05-21 21:49:32 +0800

    SecondPush

--- DB_FKPark/createSummonsTable.php
+++ DB_FKPark/createSummonsTable.php
@@ -18,9 +18,9 @@ $query1 = 'CREATE TABLE summons( ' .
           'summons_QR VARCHAR(100) NOT NULL, ' .
           'uk_ID VARCHAR(255) NOT NULL, ' .
           'vehicle_numPlate VARCHAR(10), ' .
-          'PRIMARY KEY(event_ID))' .
-          'FOREIGN KEY (uk_ID) REFERENCES unitkeselamatanstaff(uk_ID))' . 
-          'FOREIGN KEY (vehicle_numPlate) REFERENCES vehicle(vehicle_numPlate)';
+          'PRIMARY KEY(summons_ID))' .
+          'FOREIGN KEY (uk_ID) REFERENCES Unitkeselamatanstaff(uk_ID))' . 
+          'FOREIGN KEY (vehicle_numPlate) REFERENCES Vehicle(vehicle_numPlate))';
 
 if (mysqli_query($con, $query1)) {
     echo "<h3>Your summons table has been created !!!</h3>";
@@ -28,3 +28,6 @@ if (mysqli_query($con, $query1)) {
     echo "<br>";
     echo "Error creating table: " . mysqli_error($con);
 }
+
+mysqli_close($con);
+?>
