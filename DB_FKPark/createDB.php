4041af5625fdf528dd2bdbe26805f231eec95f8a
Gin-chan1 <130429674+Gin-chan1@users.noreply.github.com>
2024-05-21 15:38:00 +0800

    table

--- DB_FKPark/createDB.php
+++ DB_FKPark/createDB.php
@@ -69,6 +69,94 @@ if (mysqli_query($con, $query3)) {
     echo "Error creating table: " . mysqli_error($con);
 }
 
+// Now create the registration table with the foreign key
+$query4 = 'CREATE TABLE Registration( ' .
+          'registration_ID VARCHAR(10) NOT NULL AUTO_INCREMENT, ' .
+          'vehicle_grant VARCHAR(100) NOT NULL, ' .
+          'registration_status VARCHAR(10) NOT NULL, ' .
+          'PRIMARY KEY(registration_ID), ' .
+          'FOREIGN KEY (administrator_ID) REFERENCES administrator(administrator_ID))';
+          'FOREIGN KEY (student_ID) REFERENCES student(student_ID))';
+
+
+if (mysqli_query($con, $query3)) {
+    echo "<h3>Your registration table has been created !!!</h3>";
+} else {
+    echo "<br>";
+    echo "Error creating table: " . mysqli_error($con);
+}
+
+// Now create the vehicle table with the foreign key
+$query5 = 'CREATE TABLE Vehicle( ' .
+          'vehicle_numPlate VARCHAR(10) NOT NULL AUTO_INCREMENT, ' .
+          'vehicle_type VARCHAR(20) NOT NULL, ' .
+          'vehicle_brand VARCHAR(50) NOT NULL, ' .
+          'vehicle_transmission VARCHAR(20) NOT NULL, ' .
+          'PRIMARY KEY(vehicle_numPlate), ' .
+          'FOREIGN KEY (student_ID) REFERENCES student(student_ID))';
+
+
+if (mysqli_query($con, $query3)) {
+    echo "<h3>Your vehicle table has been created !!!</h3>";
+} else {
+    echo "<br>";
+    echo "Error creating table: " . mysqli_error($con);
+}
+
+// Now create the student table with the foreign key
+$query6 = 'CREATE TABLE Student( ' .
+          'student_ID VARCHAR(10) NOT NULL AUTO_INCREMENT, ' .
+          'student_username VARCHAR(100) NOT NULL, ' .
+          'student_password VARCHAR(100) NOT NULL, ' .
+          'student_email VARCHAR(100) NOT NULL, ' .
+          'student_age NUMBER NOT NULL, ' .
+          'student_demtot NUMBER NOT NULL, ' .
+          'PRIMARY KEY(student_ID), ' .
+          'FOREIGN KEY (administrator_ID) REFERENCES administrator(administrator_ID))';
+
+
+if (mysqli_query($con, $query3)) {
+    echo "<h3>Your student table has been created !!!</h3>";
+} else {
+    echo "<br>";
+    echo "Error creating table: " . mysqli_error($con);
+}
+
+// Now create the administrator table with the foreign key
+$query7 = 'CREATE TABLE Administrator( ' .
+          'administrator_ID VARCHAR(10) NOT NULL AUTO_INCREMENT, ' .
+          'administrator_username VARCHAR(100) NOT NULL, ' .
+          'administrator_password VARCHAR(100) NOT NULL, ' .
+          'administrator_email VARCHAR(100) NOT NULL, ' .
+          'administrator_age NUMBER NOT NULL, ' .
+          'PRIMARY KEY(administrator_ID), ' ;
+
+
+if (mysqli_query($con, $query3)) {
+    echo "<h3>Your administrator table has been created !!!</h3>";
+} else {
+    echo "<br>";
+    echo "Error creating table: " . mysqli_error($con);
+}
+
+// Now create the unitkeselamatanstaff table with the foreign key
+$query8 = 'CREATE TABLE UnitKeselamatanStaff( ' .
+          'uk_ID VARCHAR(10) NOT NULL AUTO_INCREMENT, ' .
+          'uk_username VARCHAR(100) NOT NULL, ' .
+          'uk_password VARCHAR(100) NOT NULL, ' .
+          'uk_email VARCHAR(100) NOT NULL, ' .
+          'uk_age NUMBER NOT NULL, ' .
+          'PRIMARY KEY(uk_ID), ' ;
+
+
+if (mysqli_query($con, $query3)) {
+    echo "<h3>Your unitkeselamatanstaff table has been created !!!</h3>";
+} else {
+    echo "<br>";
+    echo "Error creating table: " . mysqli_error($con);
+}
+
+
 
 mysqli_close($con);
 ?>
