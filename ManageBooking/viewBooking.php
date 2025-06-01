65770ba286e85a6c0ba88fb798fe6f361fa5ccc5
jicoon1706 <jicoon1706@gmail.com>
2025-06-02 01:16:25 +0800

    akmal_sem

--- ManageBooking/viewBooking.php
+++ ManageBooking/viewBooking.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+date_default_timezone_set('Asia/Kuala_Lumpur');
 
 // Check if the user ID is set in the session
 if (!isset($_SESSION['userID'])) {
@@ -15,7 +16,7 @@ $studentID = $_SESSION['userID'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="viewBooking.css">
+    <link rel="stylesheet" href="../ManageBooking/viewBooking.css">
     <title>Parking Booking Form</title>
 </head>
 <body>
@@ -39,28 +40,49 @@ $studentID = $_SESSION['userID'];
                 if (!$con) {
                     die('Could not connect: ' . mysqli_connect_error());
                 }
+                
 
                 // Retrieve bookings for the logged-in user
                 $query = "SELECT booking_ID, parkingSlot_name, booking_date, booking_startTime, booking_endTime 
-                          FROM booking 
-                          JOIN parkingSlot ON booking.parkingSlot_ID = parkingSlot.parkingSlot_ID 
-                          WHERE student_ID = '$studentID'";
+                            FROM booking 
+                            JOIN parkingSlot ON booking.parkingSlot_ID = parkingSlot.parkingSlot_ID 
+                            WHERE student_ID = '$studentID'
+                            ORDER BY booking_date DESC, booking_startTime DESC";
+
                 $result = mysqli_query($con, $query);
                 $counter = 1;
 
                 if ($result) {
                     while ($row = mysqli_fetch_assoc($result)) {
+                        $startDateTime = DateTime::createFromFormat('Y-m-d H:i:s', $row['booking_date'] . ' ' . $row['booking_startTime']);
+                        $currentDateTime = new DateTime();
+                        $editCancelAllowed = $currentDateTime < (clone $startDateTime)->modify('-10 minutes');
+
                         echo "<tr class='view-table-content'>
-                                <td>{$counter}</td>
-                                <td>{$row['parkingSlot_name']}</td>
-                                <td>{$row['booking_date']}</td>
-                                <td>{$row['booking_startTime']}</td>
-                                <td>{$row['booking_endTime']}</td>
-                                <td><a href='editBooking.php?bookingID={$row['booking_ID']}' class='action-btn'>Edit</a></td>
-                                <td><a href='QRBooking.php?parkingSlotName={$row['parkingSlot_name']}&bookingDate={$row['booking_date']}&startTime={$row['booking_startTime']}&endTime={$row['booking_endTime']}' class='action-btn'>View QR</a></td>
-                                <td><a href='#' onclick='confirmCancel({$row['booking_ID']})' class='action-btn'>Cancel</a></td>
-                              </tr>";
+                            <td>{$counter}</td>
+                            <td>{$row['parkingSlot_name']}</td>
+                            <td>{$row['booking_date']}</td>
+                            <td>{$row['booking_startTime']}</td>
+                            <td>{$row['booking_endTime']}</td>";
+
+                        if ($editCancelAllowed) {
+                            echo "<td><a href='editBooking.php?bookingID={$row['booking_ID']}' class='action-btn'>Edit</a></td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+
+                        echo "<td><a href='QRBooking.php?parkingSlotName={$row['parkingSlot_name']}&bookingDate={$row['booking_date']}&startTime={$row['booking_startTime']}&endTime={$row['booking_endTime']}' class='action-btn'>View QR</a></td>";
+
+                        if ($editCancelAllowed) {
+                            echo "<td><a href='#' onclick='confirmCancel({$row['booking_ID']})' class='action-btn'>Cancel</a></td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }
+
+                        echo "</tr>";
+
                         $counter++;
+
                     }
                 } else {
                     echo "Error retrieving bookings: " . mysqli_error($con);
